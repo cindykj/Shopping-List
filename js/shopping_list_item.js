fb16a17e788c25748c77b9119db41648fa6bc8ee
michelle moyer <moyer.mj@gmail.com>
2018-01-19 15:32:29 -1000

    first test passed.

--- js/shopping_list_item.js
+++ js/shopping_list_item.js
@@ -1,1 +1,9 @@
 // Code to pass shopping list tests - as item
+
+class ShoppingListItem {
+  constructor(name, description, is_done){
+    this.name = name;
+    this.description = description;
+    this.is_done = true;
+  }
+}
