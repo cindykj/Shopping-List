849daccec2231697c284742c90d48ca4235e19cb
KanKan <cindykan@KanKan.local>
2018-01-20 12:21:05 -1000

    Render test and code complete

--- js/shopping_list_item.js
+++ js/shopping_list_item.js
@@ -16,24 +16,9 @@ class ShoppingListItem {
   }
 
   render() {
+    return `<li class="completed_${this.is_done}"><span>${this.name}</span> <span>${this.description}</span></li>`;
 
-    let mainList = document.createElement('ul');
-    mainList.innerHTML = 
-    document.body.appendChild(ul)
-
-    let list = document.createElement('li');
-    list.className = `completed_`+ this.is_done;
-    mainList.appendChild(list);
-
-    let htmlName = document.createElement('span');
-    htmlName.innerHTML = this.name;
-    list.appendChild(htmlName);
-
-    let htmlDescription = document.createElement('span');
-    htmlDescription.innerHTML = this.description;    
-    list.appendChild(htmlDescription);
-
-    console.log(`${list}  ${htmlName}  ${htmlDescription}`)
+   
    
    // return `${list}`
   }
