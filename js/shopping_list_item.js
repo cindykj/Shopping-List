cd1396c080b9075b54549cd74fc68adbac947c59
KanKan <cindykan@KanKan.local>
2018-01-19 16:14:38 -1000

    Shopping_list_item code passes!

--- js/shopping_list_item.js
+++ js/shopping_list_item.js
@@ -6,4 +6,8 @@ class ShoppingListItem {
     this.description = description;
     this.is_done = false;
   }
-}
+
+  check() {
+    this.is_done = true;
+  }
+}; //shoppinglist item closing
--- js/shopping_list_test.js
+++ js/shopping_list_test.js
@@ -1,40 +1,47 @@
 // All tests in here!
 let expect = chai.expect;
 let should = chai.should();
-let shoppingListItem;
+
 // Before and after??
-beforeEach(function () {
-  shoppingListItem = new ShoppingListItem('potato', 'starchy');
-})
+describe('everything', function () {
+  let shoppingListItem;
+  let param1 = 'potato';
+  let param2 = 'starchy';
+  
 
-// ShoppingListItem
-describe('ShoppingListItem', function () {
-  it('should be a function', function () {
-    expect(ShoppingListItem).to.be.a('function');
-  });
-
-  it('should have a name', function () {
-    shoppingListItem.name.should.equal('potato');
-  });
-
-  it('should have a description', function () {
-    shoppingListItem.description.should.equal('starchy');
-  });
-
-  it('should be done', function () {
-    shoppingListItem.is_done.should.equal(false);
-  });
-
-}); //closing bracket for ShoppingListItem
-
-describe('.check', function (){
-  it('should be a function', function (){
-    expect(shoppingListItem.check).to.be.a('function');
-  });
-
-  it('should set done property to true', function (){
-    shoppingListItem.check();
-    shoppingListItem.is_done.should.equal(true);
+  beforeEach(function () {
+    shoppingListItem = new ShoppingListItem('param1', 'param2');
   })
-
-})// closing bracket for check
+  
+  // ShoppingListItem
+  describe('ShoppingListItem', function () {
+    it('should be a function', function () {
+      expect(ShoppingListItem).to.be.a('function');
+    });
+    
+    it('should have a name', function () {
+      shoppingListItem.name.should.equal('param1');
+    });
+    
+    it('should have a description', function () {
+      shoppingListItem.description.should.equal('param2');
+    });
+    
+    it('should be done', function () {
+      shoppingListItem.is_done.should.equal(false);
+    });
+    
+  }); //closing bracket for ShoppingListItem
+  
+  describe('.check', function (){
+    it('should be a function', function (){
+      expect(shoppingListItem.check).to.be.a('function');
+    });
+    
+    it('should set done property to true', function (){
+      shoppingListItem.check();
+      shoppingListItem.is_done.should.equal(true);
+    })
+    
+  })// closing bracket for check
+}); //closing for 'everything'
